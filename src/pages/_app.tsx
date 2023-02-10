a160401def90aba33ef63419bf0dd027893a99c5
eoldam <eoldamstory@gmail.com>
2023-02-10 18:30:47 +0900

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,50 @@
 import type { AppProps } from "next/app";
-import { appWithTranslation } from "next-i18next";
 import { ThemeProvider } from "next-themes";
+import { appWithTranslation, useTranslation } from "next-i18next";
+import { DefaultSeo } from "next-seo";
 import { CursorProvider } from "@/context/cursor";
 import { CommonProvider } from "@/context/common";
-import "@/styles/globals.css";
 import { ProjectProvider } from "@/context/project";
+import "@/styles/globals.css";
+
+const DEFAULT_SEO = {
+  title: "정재우의 포트폴리오",
+  description: "개발자로 살아보자! 늦깎이 개발자 정재우의 포트폴리오",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://portfolio-ten-blond-73.vercel.app/",
+    title: "정재우의 포트폴리오",
+    site_name: "JW's Portfolio",
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/korea-storage.appspot.com/o/portfolio-og-thumbnail.png?alt=media&token=39751038-613f-4adc-be1e-29d7d67c959b",
+        width: 1200,
+        height: 630,
+        alt: "JW's portfolio",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
 
 export default appWithTranslation(function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
-      <CursorProvider>
-        <CommonProvider>
-          <ProjectProvider>
-            <Component {...pageProps} />
-          </ProjectProvider>
-        </CommonProvider>
-      </CursorProvider>
-    </ThemeProvider>
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <ThemeProvider attribute="class" enableSystem={false}>
+        <CursorProvider>
+          <CommonProvider>
+            <ProjectProvider>
+              <Component {...pageProps} />
+            </ProjectProvider>
+          </CommonProvider>
+        </CursorProvider>
+      </ThemeProvider>
+    </>
   );
 });
